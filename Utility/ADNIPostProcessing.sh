f71d0361a9ef56d48396aa3c7c331aab5d5f2595
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-03-04 11:44:41 -0600

    Directory selection logic

--- Utility/ADNIPostProcessing.sh
+++ Utility/ADNIPostProcessing.sh
@@ -4,26 +4,32 @@
 CURRENT_DATETIME=$(date +'%Y-%m-%d_%H-%M-%S') # Date and time will be appended to the log file so multiple can be run keeping data separate
 LOG_OUTPUT_DIR=${HOME}/Scripts/MyScripts/logs
 LOG_OUTPUT=${LOG_OUTPUT_DIR}/$(basename "$0")_${CURRENT_DATETIME}.log # name and location of log file
-SUBJECTS=() # Array of subject numbers to be processed
-LEVELS=6 # Levels for MSM
 
 ######### CHANGE AS NEEDED
 DATASET=/N/project/aMSM_AD/ADNI/HCP/MSM # Folder containing subject data
+BASE_SCENE=${DATASET}/base.scene # Master scene file location
 ACCOUNT="r00540" # Slurm allocation to use
 
 ########## ENSURE THAT OUTPUT AND LOG DIRS EXISTS
-#mkdir -p ${LOG_OUTPUT_DIR}
+mkdir -p ${LOG_OUTPUT_DIR}
 
 ########## BEGIN LOGGING
-#exec > >(tee -a "${LOG_OUTPUT}") 2>&1
+exec > >(tee -a "${LOG_OUTPUT}") 2>&1
 
 DIRECTORIES=($(ls ${DATASET}))
 
 for DIR in ${DIRECTORIES[@]}; do
+    echo "***************************************************************************"
+    echo "FIND ALL SUBJECT DIRECTORIES"
+    echo "***************************************************************************"
+    SUBJECT_DIR=${DATASET}/${DIR}
     SUBJECT=$(echo ${DIR} | cut -d "_" -f 1)
     TIME1=$(echo ${DIR} | cut -d "_" -f 2)
     TIME2=$(echo ${DIR} | cut -d "_" -f 4)
+    echo "DIRECTORIES LOCATED. RELEVANT DIRECTORES:"
 
-    echo "${SUBJECT}, ${TIME1}, ${TIME2}"
+    if [ ${TIME1} == "BL" ]; then
+        echo ${SUBJECT_DIR}
+    fi
 done
 
