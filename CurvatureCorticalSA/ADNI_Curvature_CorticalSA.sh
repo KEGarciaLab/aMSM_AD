98ed379541a5600132dcae745b22c5b324aa5227
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-03-25 07:18:58 -0500

    Completed issues #1 and #2

--- CurvatureCorticalSA/ADNI_Curvature_CorticalSA.sh
+++ CurvatureCorticalSA/ADNI_Curvature_CorticalSA.sh
@@ -141,6 +141,7 @@ for DIR in ${DIRECTORIES[@]}; do
         ######## CALCULATE GAUSS CURVE
         echo
         echo "CALCULATING GAUSS CURVE"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         wb_command -surface-curvature ${SUBJECT_LYS_CP} -gauss ${L_CP_OUT}${TIME1}.gauss_curve.func.gii
         echo "CPGRID YOUNGER LEFT HEMISPHERE COMPLETE. SAVED AT ${L_CP_OUT}${TIME1}.gauss_curve.func.gii"
@@ -168,6 +169,7 @@ for DIR in ${DIRECTORIES[@]}; do
         ######## CALCULATE MEAN CURVE
         echo
         echo "CALCULATING MEAN CURVE"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         wb_command -surface-curvature ${SUBJECT_LYS_CP} -mean ${L_CP_OUT}${TIME1}.mean_curve.func.gii
         echo "CPGRID YOUNGER LEFT HEMISPHERE COMPLETE. SAVED AT ${L_CP_OUT}${TIME1}.mean_curve.func.gii"
@@ -195,6 +197,7 @@ for DIR in ${DIRECTORIES[@]}; do
         ######## KMAX
         echo
         echo "CALCULATING KMAX"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         wb_command -metric-math '(KH+sqrt(KH^2-KG))' ${L_CP_OUT}${TIME1}.kmax.func.gii -fixnan 0 -var KH ${L_CP_OUT}${TIME1}.mean_curve.func.gii -var KG ${L_CP_OUT}${TIME1}.gauss_curve.func.gii
         echo "CPGRID YOUNGER LEFT HEMISPHERE COMPLETE. SAVED AT ${L_CP_OUT}${TIME1}.kmax.func.gii"
@@ -222,6 +225,7 @@ for DIR in ${DIRECTORIES[@]}; do
         ######## KMIN
         echo
         echo "CALCULATING KMIN"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         wb_command -metric-math '(KH-sqrt(KH^2-KG))' ${L_CP_OUT}${TIME1}.kmin.func.gii -fixnan 0 -var KH ${L_CP_OUT}${TIME1}.mean_curve.func.gii -var KG ${L_CP_OUT}${TIME1}.gauss_curve.func.gii
         echo "CPGRID YOUNGER LEFT HEMISPHERE COMPLETE. SAVED AT ${L_CP_OUT}${TIME1}.kmin.func.gii"
@@ -249,6 +253,7 @@ for DIR in ${DIRECTORIES[@]}; do
         ######## K1
         echo
         echo "CALCULATING K1"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         wb_command -metric-math '(max(abs(Kmax),abs(Kmin))*((Kmax+Kmin)/(abs(Kmax+Kmin))))' ${L_CP_OUT}${TIME1}.K1.func.gii -fixnan 0 -var Kmax ${L_CP_OUT}${TIME1}.kmax.func.gii -var Kmin ${L_CP_OUT}${TIME1}.kmin.func.gii
         echo "CPGRID YOUNGER LEFT HEMISPHERE COMPLETE. SAVED AT ${L_CP_OUT}${TIME1}.K1.func.gii"
@@ -276,6 +281,7 @@ for DIR in ${DIRECTORIES[@]}; do
         ######## K2
         echo
         echo "CALCULATING K2"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         wb_command -metric-math 'KG/K1' ${L_CP_OUT}${TIME1}.K2.func.gii -fixnan 0 -var KG ${L_CP_OUT}${TIME1}.gauss_curve.func.gii -var K1 ${L_CP_OUT}${TIME1}.K1.func.gii
         echo "CPGRID YOUNGER LEFT HEMISPERE COMPLETE. SAVED AT ${L_CP_OUT}${TIME1}.K2.func.gii"
@@ -304,6 +310,7 @@ for DIR in ${DIRECTORIES[@]}; do
         ###### SULCI
         echo
         echo "SEPERATING SULCI"
+        echo "***************************************************************************"
         #### CPGRID YOUNGER
         wb_command -metric-math '(K1*(K1<0))' ${L_CP_OUT}${TIME1}.K1.sulci.func.gii -fixnan 0 -var K1 ${L_CP_OUT}${TIME1}.K1.func.gii
         echo "CPGRID YOUNGER LEFT HEMISPHERE COMPLETE. SAVED AT ${L_CP_OUT}${TIME1}.K1.sulci.func.gii"
@@ -331,6 +338,7 @@ for DIR in ${DIRECTORIES[@]}; do
         ###### GYRI
         echo
         echo "SEPERATING GYRI"
+        echo "***************************************************************************"
         #### CPGRID YOUNGER
         wb_command -metric-math '(K1*(K1>0))' ${L_CP_OUT}${TIME1}.K1.gyri.func.gii -fixnan 0 -var K1 ${L_CP_OUT}${TIME1}.K1.func.gii
         echo "CPGRID YOUNGER LEFT HEMISPHERE COMPLETE. SAVED AT ${L_CP_OUT}${TIME1}.K1.gyri.func.gii"
@@ -357,109 +365,115 @@ for DIR in ${DIRECTORIES[@]}; do
 
         ########## MEAN GYRI
         echo
-        echo "CALCULATING MEAN GYRI"
+        echo "***************************************************************************"
+        echo "BEGIN CALCULATING MEAN GYRI"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         CP_LY_SUM_GYRI=$(wb_command -metric-stats ${L_CP_OUT}${TIME1}.K1.gyri.func.gii -reduce SUM)
         CP_LY_COUNT_GYRI=$(wb_command -metric-stats ${L_CP_OUT}${TIME1}.K1.gyri.func.gii -reduce COUNT_NONZERO)
-        CP_LY_MEAN_GYRI=$(echo "scale=20; ${CP_LY_SUM_GYRI}/${CP_LY_COUNT_GYRI}" | bc)
-        CP_LY_MEAN_GYRI=$(printf "%022.20f" ${CP_LY_MEAN_GYRI})
+        CP_LY_MEAN_GYRI=$(echo "scale=9; ${CP_LY_SUM_GYRI}/${CP_LY_COUNT_GYRI}" | bc)
+        CP_LY_MEAN_GYRI=$(printf "%011.9f" ${CP_LY_MEAN_GYRI})
         echo "CPGRID LEFT YOUNGER MEAN GYRI=${CP_LY_MEAN_GYRI}"
         CP_RY_SUM_GYRI=$(wb_command -metric-stats ${R_CP_OUT}${TIME1}.K1.gyri.func.gii -reduce SUM)
         CP_RY_COUNT_GYRI=$(wb_command -metric-stats ${R_CP_OUT}${TIME1}.K1.gyri.func.gii -reduce COUNT_NONZERO)
-        CP_RY_MEAN_GYRI=$(echo "scale=20; ${CP_RY_SUM_GYRI}/${CP_RY_COUNT_GYRI}" | bc)
-        CP_RY_MEAN_GYRI=$(printf "%022.20f" ${CP_RY_MEAN_GYRI})
+        CP_RY_MEAN_GYRI=$(echo "scale=9; ${CP_RY_SUM_GYRI}/${CP_RY_COUNT_GYRI}" | bc)
+        CP_RY_MEAN_GYRI=$(printf "%011.9f" ${CP_RY_MEAN_GYRI})
         echo "CPGRID RIGHT YOUNGER MEAN GYRI=${CP_RY_MEAN_GYRI}"
 
         ###### CPGRID OLDER
         CP_LO_SUM_GYRI=$(wb_command -metric-stats ${L_CP_OUT}${TIME2}.K1.gyri.func.gii -reduce SUM)
         CP_LO_COUNT_GYRI=$(wb_command -metric-stats ${L_CP_OUT}${TIME2}.K1.gyri.func.gii -reduce COUNT_NONZERO)
-        CP_LO_MEAN_GYRI=$(echo "scale=20; ${CP_LO_SUM_GYRI}/${CP_LO_COUNT_GYRI}" | bc)
-        CP_LO_MEAN_GYRI=$(printf "%022.20f" ${CP_LO_MEAN_GYRI})
+        CP_LO_MEAN_GYRI=$(echo "scale=9; ${CP_LO_SUM_GYRI}/${CP_LO_COUNT_GYRI}" | bc)
+        CP_LO_MEAN_GYRI=$(printf "%011.9f" ${CP_LO_MEAN_GYRI})
         echo "CPGRID LEFT OLDER MEAN GYRI=${CP_LO_MEAN_GYRI}"
         CP_RO_SUM_GYRI=$(wb_command -metric-stats ${R_CP_OUT}${TIME2}.K1.gyri.func.gii -reduce SUM)
         CP_RO_COUNT_GYRI=$(wb_command -metric-stats ${R_CP_OUT}${TIME2}.K1.gyri.func.gii -reduce COUNT_NONZERO)
-        CP_RO_MEAN_GYRI=$(echo "scale=20; ${CP_RO_SUM_GYRI}/${CP_RO_COUNT_GYRI}" | bc)
-        CP_RO_MEAN_GYRI=$(printf "%022.20f" ${CP_RO_MEAN_GYRI})
+        CP_RO_MEAN_GYRI=$(echo "scale=9; ${CP_RO_SUM_GYRI}/${CP_RO_COUNT_GYRI}" | bc)
+        CP_RO_MEAN_GYRI=$(printf "%011.9f" ${CP_RO_MEAN_GYRI})
         echo "CPGRID RIGHT OLDER MEAN GYRI=${CP_RO_MEAN_GYRI}"
         
         ###### ANATGRID YOUNGER
         ANAT_LY_SUM_GYRI=$(wb_command -metric-stats ${L_ANAT_OUT}${TIME1}.K1.gyri.func.gii -reduce SUM)
         ANAT_LY_COUNT_GYRI=$(wb_command -metric-stats ${L_ANAT_OUT}${TIME1}.K1.gyri.func.gii -reduce COUNT_NONZERO)
-        ANAT_LY_MEAN_GYRI=$(echo "scale=20; ${ANAT_LY_SUM_GYRI}/${ANAT_LY_COUNT_GYRI}" | bc)
-        ANAT_LY_MEAN_GYRI=$(printf "%022.20f" ${ANAT_LY_MEAN_GYRI})
+        ANAT_LY_MEAN_GYRI=$(echo "scale=9; ${ANAT_LY_SUM_GYRI}/${ANAT_LY_COUNT_GYRI}" | bc)
+        ANAT_LY_MEAN_GYRI=$(printf "%011.9f" ${ANAT_LY_MEAN_GYRI})
         echo "ANATGRID LEFT YOUNGER MEAN GYRI=${ANAT_LY_MEAN_GYRI}"
         ANAT_RY_SUM_GYRI=$(wb_command -metric-stats ${R_ANAT_OUT}${TIME1}.K1.gyri.func.gii -reduce SUM)
         ANAT_RY_COUNT_GYRI=$(wb_command -metric-stats ${R_ANAT_OUT}${TIME1}.K1.gyri.func.gii -reduce COUNT_NONZERO)
-        ANAT_RY_MEAN_GYRI=$(echo "scale=20; ${ANAT_RY_SUM_GYRI}/${ANAT_RY_COUNT_GYRI}" | bc)
-        ANAT_RY_MEAN_GYRI=$(printf "%022.20f" ${ANAT_RY_MEAN_GYRI})
+        ANAT_RY_MEAN_GYRI=$(echo "scale=9; ${ANAT_RY_SUM_GYRI}/${ANAT_RY_COUNT_GYRI}" | bc)
+        ANAT_RY_MEAN_GYRI=$(printf "%011.9f" ${ANAT_RY_MEAN_GYRI})
         echo "ANATGRID RIGHT YOUNGER MEAN GYRI=${ANAT_RY_MEAN_GYRI}"
         
         ###### ANATGRID OLDER
         ANAT_LO_SUM_GYRI=$(wb_command -metric-stats ${L_ANAT_OUT}${TIME2}.K1.gyri.func.gii -reduce SUM)
         ANAT_LO_COUNT_GYRI=$(wb_command -metric-stats ${L_ANAT_OUT}${TIME2}.K1.gyri.func.gii -reduce COUNT_NONZERO)
-        ANAT_LO_MEAN_GYRI=$(echo "scale=20; ${ANAT_LO_SUM_GYRI}/${ANAT_LO_COUNT_GYRI}" | bc)
-        ANAT_LO_MEAN_GYRI=$(printf "%022.20f" ${ANAT_LO_MEAN_GYRI})
+        ANAT_LO_MEAN_GYRI=$(echo "scale=9; ${ANAT_LO_SUM_GYRI}/${ANAT_LO_COUNT_GYRI}" | bc)
+        ANAT_LO_MEAN_GYRI=$(printf "%011.9f" ${ANAT_LO_MEAN_GYRI})
         echo "ANATGRID LEFT OLDER MEAN GYRI=${ANAT_LO_MEAN_GYRI}"
         ANAT_RO_SUM_GYRI=$(wb_command -metric-stats ${R_ANAT_OUT}${TIME2}.K1.gyri.func.gii -reduce SUM)
         ANAT_RO_COUNT_GYRI=$(wb_command -metric-stats ${R_ANAT_OUT}${TIME2}.K1.gyri.func.gii -reduce COUNT_NONZERO)
-        ANAT_RO_MEAN_GYRI=$(echo "scale=20; ${ANAT_RO_SUM_GYRI}/${ANAT_RO_COUNT_GYRI}" | bc)
-        ANAT_RO_MEAN_GYRI=$(printf "%022.20f" ${ANAT_RO_MEAN_GYRI})
+        ANAT_RO_MEAN_GYRI=$(echo "scale=9; ${ANAT_RO_SUM_GYRI}/${ANAT_RO_COUNT_GYRI}" | bc)
+        ANAT_RO_MEAN_GYRI=$(printf "%011.9f" ${ANAT_RO_MEAN_GYRI})
         echo "ANATGRID RIGHT OLDER MEAN GYRI=${ANAT_RO_MEAN_GYRI}"
         
         ########## MEAN SULCI
         echo
-        echo "CALCULATING MEAN SULCI"
+        echo "***************************************************************************"
+        echo "BEGIN CALCULATING MEAN SULCI"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         CP_LY_SUM_SULCI=$(wb_command -metric-stats ${L_CP_OUT}${TIME1}.K1.sulci.func.gii -reduce SUM)
         CP_LY_COUNT_SULCI=$(wb_command -metric-stats ${L_CP_OUT}${TIME1}.K1.sulci.func.gii -reduce COUNT_NONZERO)
-        CP_LY_MEAN_SULCI=$(echo "scale=20; ${CP_LY_SUM_SULCI}/${CP_LY_COUNT_SULCI}" | bc)
-        CP_LY_MEAN_SULCI=$(printf "%022.20f" ${CP_LY_MEAN_SULCI})
+        CP_LY_MEAN_SULCI=$(echo "scale=9; ${CP_LY_SUM_SULCI}/${CP_LY_COUNT_SULCI}" | bc)
+        CP_LY_MEAN_SULCI=$(printf "%011.9f" ${CP_LY_MEAN_SULCI})
         echo "CPGRID LEFT YOUNGER MEAN SULCI=${CP_LY_MEAN_SULCI}"
         CP_RY_SUM_SULCI=$(wb_command -metric-stats ${R_CP_OUT}${TIME1}.K1.sulci.func.gii -reduce SUM)
         CP_RY_COUNT_SULCI=$(wb_command -metric-stats ${R_CP_OUT}${TIME1}.K1.sulci.func.gii -reduce COUNT_NONZERO)
-        CP_RY_MEAN_SULCI=$(echo "scale=20; ${CP_RY_SUM_SULCI}/${CP_RY_COUNT_SULCI}" | bc)
-        CP_RY_MEAN_SULCI=$(printf "%022.20f" ${CP_RY_MEAN_SULCI})
+        CP_RY_MEAN_SULCI=$(echo "scale=9; ${CP_RY_SUM_SULCI}/${CP_RY_COUNT_SULCI}" | bc)
+        CP_RY_MEAN_SULCI=$(printf "%011.9f" ${CP_RY_MEAN_SULCI})
         echo "CPGRID RIGHT YOUNGER MEAN SULCI=${CP_RY_MEAN_SULCI}"
 
         ###### CPGRID OLDER
         CP_LO_SUM_SULCI=$(wb_command -metric-stats ${L_CP_OUT}${TIME2}.K1.sulci.func.gii -reduce SUM)
         CP_LO_COUNT_SULCI=$(wb_command -metric-stats ${L_CP_OUT}${TIME2}.K1.sulci.func.gii -reduce COUNT_NONZERO)
-        CP_LO_MEAN_SULCI=$(echo "scale=20; ${CP_LO_SUM_SULCI}/${CP_LO_COUNT_SULCI}" | bc)
-        CP_LO_MEAN_SULCI=$(printf "%022.20f" ${CP_LO_MEAN_SULCI})
+        CP_LO_MEAN_SULCI=$(echo "scale=9; ${CP_LO_SUM_SULCI}/${CP_LO_COUNT_SULCI}" | bc)
+        CP_LO_MEAN_SULCI=$(printf "%011.9f" ${CP_LO_MEAN_SULCI})
         echo "CPGRID LEFT OLDER MEAN SULCI=${CP_LO_MEAN_SULCI}"
         CP_RO_SUM_SULCI=$(wb_command -metric-stats ${R_CP_OUT}${TIME2}.K1.sulci.func.gii -reduce SUM)
         CP_RO_COUNT_SULCI=$(wb_command -metric-stats ${R_CP_OUT}${TIME2}.K1.sulci.func.gii -reduce COUNT_NONZERO)
-        CP_RO_MEAN_SULCI=$(echo "scale=20; ${CP_RO_SUM_SULCI}/${CP_RO_COUNT_SULCI}" | bc)
-        CP_RO_MEAN_SULCI=$(printf "%022.20f" ${CP_RO_MEAN_SULCI})
+        CP_RO_MEAN_SULCI=$(echo "scale=9; ${CP_RO_SUM_SULCI}/${CP_RO_COUNT_SULCI}" | bc)
+        CP_RO_MEAN_SULCI=$(printf "%011.9f" ${CP_RO_MEAN_SULCI})
         echo "CPGRID RIGHT OLDER MEAN SULCI=${CP_RO_MEAN_SULCI}"
         
         ###### ANATGRID YOUNGER
         ANAT_LY_SUM_SULCI=$(wb_command -metric-stats ${L_ANAT_OUT}${TIME1}.K1.sulci.func.gii -reduce SUM)
         ANAT_LY_COUNT_SULCI=$(wb_command -metric-stats ${L_ANAT_OUT}${TIME1}.K1.sulci.func.gii -reduce COUNT_NONZERO)
-        ANAT_LY_MEAN_SULCI=$(echo "scale=20; ${ANAT_LY_SUM_SULCI}/${ANAT_LY_COUNT_SULCI}" | bc)
-        ANAT_LY_MEAN_SULCI=$(printf "%022.20f" ${ANAT_LY_MEAN_SULCI})
+        ANAT_LY_MEAN_SULCI=$(echo "scale=9; ${ANAT_LY_SUM_SULCI}/${ANAT_LY_COUNT_SULCI}" | bc)
+        ANAT_LY_MEAN_SULCI=$(printf "%011.9f" ${ANAT_LY_MEAN_SULCI})
         echo "ANATGRID LEFT YOUNGER MEAN SULCI=${ANAT_LY_MEAN_SULCI}"
         ANAT_RY_SUM_SULCI=$(wb_command -metric-stats ${R_ANAT_OUT}${TIME1}.K1.sulci.func.gii -reduce SUM)
         ANAT_RY_COUNT_SULCI=$(wb_command -metric-stats ${R_ANAT_OUT}${TIME1}.K1.sulci.func.gii -reduce COUNT_NONZERO)
-        ANAT_RY_MEAN_SULCI=$(echo "scale=20; ${ANAT_RY_SUM_SULCI}/${ANAT_RY_COUNT_SULCI}" | bc)
-        ANAT_RY_MEAN_SULCI=$(printf "%022.20f" ${ANAT_RY_MEAN_SULCI})
+        ANAT_RY_MEAN_SULCI=$(echo "scale=9; ${ANAT_RY_SUM_SULCI}/${ANAT_RY_COUNT_SULCI}" | bc)
+        ANAT_RY_MEAN_SULCI=$(printf "%011.9f" ${ANAT_RY_MEAN_SULCI})
         echo "ANATGRID RIGHT YOUNGER MEAN SULCI=${ANAT_RY_MEAN_SULCI}"
         
         ###### ANATGRID OLDER
         ANAT_LO_SUM_SULCI=$(wb_command -metric-stats ${L_ANAT_OUT}${TIME2}.K1.sulci.func.gii -reduce SUM)
         ANAT_LO_COUNT_SULCI=$(wb_command -metric-stats ${L_ANAT_OUT}${TIME2}.K1.sulci.func.gii -reduce COUNT_NONZERO)
-        ANAT_LO_MEAN_SULCI=$(echo "scale=20; ${ANAT_LO_SUM_SULCI}/${ANAT_LO_COUNT_SULCI}" | bc)
-        ANAT_LO_MEAN_SULCI=$(printf "%022.20f" ${ANAT_LO_MEAN_SULCI})
+        ANAT_LO_MEAN_SULCI=$(echo "scale=9; ${ANAT_LO_SUM_SULCI}/${ANAT_LO_COUNT_SULCI}" | bc)
+        ANAT_LO_MEAN_SULCI=$(printf "%011.9f" ${ANAT_LO_MEAN_SULCI})
         echo "ANATGRID LEFT OLDER MEAN SULCI=${ANAT_LO_MEAN_SULCI}"
         ANAT_RO_SUM_SULCI=$(wb_command -metric-stats ${R_ANAT_OUT}${TIME2}.K1.sulci.func.gii -reduce SUM)
         ANAT_RO_COUNT_SULCI=$(wb_command -metric-stats ${R_ANAT_OUT}${TIME2}.K1.sulci.func.gii -reduce COUNT_NONZERO)
-        ANAT_RO_MEAN_SULCI=$(echo "scale=20; ${ANAT_RO_SUM_SULCI}/${ANAT_RO_COUNT_SULCI}" | bc)
-        ANAT_RO_MEAN_SULCI=$(printf "%022.20f" ${ANAT_RO_MEAN_SULCI})
+        ANAT_RO_MEAN_SULCI=$(echo "scale=9; ${ANAT_RO_SUM_SULCI}/${ANAT_RO_COUNT_SULCI}" | bc)
+        ANAT_RO_MEAN_SULCI=$(printf "%011.9f" ${ANAT_RO_MEAN_SULCI})
         echo "ANATGRID RIGHT OLDER MEAN SULCI=${ANAT_RO_MEAN_SULCI}"
 
         ########## K2 VARIANCE
         echo
-        echo "CALCULATING K2 VARIANCE"
+        echo "***************************************************************************"
+        echo "BEGIN CALCULATING K2 VARIANCE"
+        echo "***************************************************************************"
         ###### CPGRID YOUNGER
         CP_LY_K2_VARIANCE=$(wb_command -metric-stats ${L_CP_OUT}${TIME1}.K2.func.gii -reduce VARIANCE)
         CP_RY_K2_VARIANCE=$(wb_command -metric-stats ${R_CP_OUT}${TIME1}.K2.func.gii -reduce VARIANCE)
@@ -484,15 +498,52 @@ for DIR in ${DIRECTORIES[@]}; do
         echo "ANATGRID LEFT OLDER K2 VARIANCE=${ANAT_LO_K2_VARIANCE}"
         echo "ANATGRID RIGHT OLDER K2 VARIANCE=${ANAT_RO_K2_VARIANCE}"
 
+        ########## GENERATE DIFFERENCE MAPS
+        echo
+        echo "***************************************************************************"
+        echo "BEGIN GENERATING DIFFERENCE MAPS"
+        echo "***************************************************************************"
+        ######## CPGRID SULCI
+        wb_command -metric-math '((K1_2-K1_1)*(K1_1<0))' ${L_CP_OUT}diff.sulci.func.gii -var K1_1 ${L_CP_OUT}${TIME1}.K1.func.gii -var K1_2 ${L_CP_OUT}${TIME2}.K1.func.gii
+        echo "CPGRID LEFT SULCI COMPLETED, SAVED AT ${L_CP_OUT}diff.sulci.func.gii"
+        wb_command -metric-math '((K1_2-K1_1)*(K1_1<0))' ${R_CP_OUT}diff.sulci.func.gii -var K1_1 ${R_CP_OUT}${TIME1}.K1.func.gii -var K1_2 ${R_CP_OUT}${TIME2}.K1.func.gii
+        echo "CPGRID RIGHT SULCI COMPLETED, SAVED AT ${R_CP_OUT}diff.sulci.func.gii"
+
+        ######## CPGRID GYRI
+        wb_command -metric-math '((K1_2-K1_1)*(K1_1>0))' ${L_CP_OUT}diff.gyri.func.gii -var K1_1 ${L_CP_OUT}${TIME1}.K1.func.gii -var K1_2 ${L_CP_OUT}${TIME2}.K1.func.gii
+        echo "CPGRID LEFT GYRI COMPLETED, SAVED AT ${L_CP_OUT}diff.gyri.func.gii"
+        wb_command -metric-math '((K1_2-K1_1)*(K1_1>0))' ${R_CP_OUT}diff.gyri.func.gii -var K1_1 ${R_CP_OUT}${TIME1}.K1.func.gii -var K1_2 ${R_CP_OUT}${TIME2}.K1.func.gii
+        echo "CPGRID RIGHT GYRI COMPLETED, SAVED AT ${R_CP_OUT}diff.gyri.func.gii"
+
+        ######## ANATGRID SULCI
+        wb_command -metric-math '((K1_2-K1_1)*(K1_1<0))' ${L_ANAT_OUT}diff.sulci.func.gii -var K1_1 ${L_ANAT_OUT}${TIME1}.K1.func.gii -var K1_2 ${L_ANAT_OUT}${TIME2}.K1.func.gii
+        echo "ANATGRID LEFT SULCI COMPLETED, SAVED AT ${L_ANAT_OUT}diff.sulci.func.gii"
+        wb_command -metric-math '((K1_2-K1_1)*(K1_1<0))' ${R_ANAT_OUT}diff.sulci.func.gii -var K1_1 ${R_ANAT_OUT}${TIME1}.K1.func.gii -var K1_2 ${R_ANAT_OUT}${TIME2}.K1.func.gii
+        echo "ANATGRID RIGHT SULCI COMPLETED, SAVED AT ${R_ANAT_OUT}diff.sulci.func.gii"
+
+        ######## ANATGRID GYRI
+        wb_command -metric-math '((K1_2-K1_1)*(K1_1>0))' ${L_ANAT_OUT}diff.gyri.func.gii -var K1_1 ${L_ANAT_OUT}${TIME1}.K1.func.gii -var K1_2 ${L_ANAT_OUT}${TIME2}.K1.func.gii
+        echo "ANATGRID LEFT GYRI COMPLETED, SAVED AT ${L_ANAT_OUT}diff.gyri.func.gii"
+        wb_command -metric-math '((K1_2-K1_1)*(K1_1>0))' ${R_ANAT_OUT}diff.gyri.func.gii -var K1_1 ${R_ANAT_OUT}${TIME1}.K1.func.gii -var K1_2 ${R_ANAT_OUT}${TIME2}.K1.func.gii
+        echo "ANATGRID RIGHT GYRI COMPLETED, SAVED AT ${R_ANAT_OUT}diff.gyri.func.gii"
+
         ########## ADD DATA TO CSV
+        echo
+        echo "***************************************************************************"
+        echo "ADDING DATA TO CSV FILES"
+        echo "***************************************************************************"
         CP_YOUNGER_DATA="${SUBJECT},${TIME1},${CP_RY_CORTICAL_SA},${CP_LY_CORTICAL_SA},${CP_RY_MEAN_GYRI},${CP_LY_MEAN_GYRI},${CP_RY_MEAN_SULCI},${CP_LY_MEAN_SULCI},${CP_RY_K2_VARIANCE},${CP_LY_K2_VARIANCE}"
         CP_OLDER_DATA="${SUBJECT},${TIME2},${CP_RO_CORTICAL_SA},${CP_LO_CORTICAL_SA},${CP_RO_MEAN_GYRI},${CP_LO_MEAN_GYRI},${CP_RO_MEAN_SULCI},${CP_LO_MEAN_SULCI},${CP_RO_K2_VARIANCE},${CP_LO_K2_VARIANCE}"
         ANAT_YOUNGER_DATA="${SUBJECT},${TIME1},${ANAT_RY_CORTICAL_SA},${ANAT_LY_CORTICAL_SA},${ANAT_RY_MEAN_GYRI},${ANAT_LY_MEAN_GYRI},${ANAT_RY_MEAN_SULCI},${ANAT_LY_MEAN_SULCI},${ANAT_RY_K2_VARIANCE},${ANAT_LY_K2_VARIANCE}"
         ANAT_OLDER_DATA="${SUBJECT},${TIME2},${ANAT_RO_CORTICAL_SA},${ANAT_LO_CORTICAL_SA},${ANAT_RO_MEAN_GYRI},${ANAT_LO_MEAN_GYRI},${ANAT_RO_MEAN_SULCI},${ANAT_LO_MEAN_SULCI},${ANAT_RO_K2_VARIANCE},${ANAT_LO_K2_VARIANCE}"
 
         echo "${CP_YOUNGER_DATA}">>"${CP_OUTPUT}"
+        echo "YOUNGER DATA AT CPGRID ADDED TO CSV ${CP_OUTPUT}"
         echo "${CP_OLDER_DATA}">>"${CP_OUTPUT}"
+        echo "OLDER DATA AT CPGRID ADDED TO CSV ${CP_OUTPUT}"
         echo "${ANAT_YOUNGER_DATA}">>"${ANAT_OUTPUT}"
+        echo "YOUNGER DATA AT ANATGRID ADDED TO CSV ${ANAT_OUTPUT}"
         echo "${ANAT_OLDER_DATA}">>"${ANAT_OUTPUT}"
+        echo "OLDER DATA AT ANATGRID ADDED TO CSV ${ANAT_OUTPUT}"
     fi
 done
