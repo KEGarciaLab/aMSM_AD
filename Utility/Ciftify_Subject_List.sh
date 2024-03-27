b1164ac1d1665378c50ddcb5bfb3b67842fd384c
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-03-27 10:53:02 -0500

    Added flag for IADRC data, reverted name. See #7

--- Utility/Ciftify_Subject_List.sh
+++ Utility/Ciftify_Subject_List.sh
@@ -4,11 +4,12 @@ LOG_OUTPUT_DIR=${HOME}/Scripts/MyScripts/logs # Dir for log file
 LOG_OUTPUT=${LOG_OUTPUT_DIR}/$(basename "$0")_${CURRENT_DATETIME}.log # name and location of log file
 
 ########## CHANGE AS NEEDED
+DATASET_NAME="IADRC" # Name of dataset being used, must be ADNI or IADRC
 DATASET=/N/project/ADNI_Processing/ADNI_FS6_ADSP/FINAL_FOR_EXTRACTION # Folder containing subject data
 CURRENT_DATETIME=$(date +'%Y-%m-%d_%H-%M-%S') # Date and time will be appended to the text file so multiple runs keep data separate
 OUTPUT_DIR=${HOME}/Scripts/MyScripts/Output/$(basename "$0") # Output location
 OUTPUT_FILE=${OUTPUT_DIR}/subject_numbers_${CURRENT_DATETIME}.txt # Output file name
-SUBJECTS="0056 0059 0069 0127 0156 0173 0210 0229 0296 0377"
+SUBJECTS="0056 0059 0069 0127 0156 0173 0210 0229 0296 0377" # Subjects to get full folder name for
 
 ########## ENSURE THAT OUTPUT AND LOG DIRS EXISTS
 mkdir -p ${LOG_OUTPUT_DIR}
@@ -18,7 +19,13 @@ mkdir -p ${OUTPUT_DIR}
 exec > >(tee -a "${LOG_OUTPUT}") 2>&1
 
 ########## ITERATE OVER EACH SUBJECT AND TIMEPOINT COMBO
-for SUBJECT in ${SUBJECTS}; do
-    echo "SEARCHING FOR FILES FOR SUBJECT ${SUBJECT}"
-    find ${DATASET} -maxdepth 1 -type d -name "*_S_${SUBJECT}_*" -exec basename {} \; >> ${OUTPUT_FILE}
-done
+if [ "${DATASET_NAME}" == "ADNI" ]; then
+    for SUBJECT in ${SUBJECTS}; do
+        echo "SEARCHING FOR FILES FOR SUBJECT ${SUBJECT}"
+        find ${DATASET} -maxdepth 1 -type d -name "*_S_${SUBJECT}_*" -exec basename {} \; >> ${OUTPUT_FILE}
+    done
+elif [ "${DATASET_NAME}" == "IADRC" ]; then
+    for SUBJECT in ${SUBJECTS}; do
+        echo "SEARCHING FOR FILES FOR SUBJECT ${SUBJECT}"
+        find ${DATASET} -maxdepth 1 -type d -name "${SUBJECT}_*" -exec basename {} \; >> ${OUTPUT_FILE}
+    done
