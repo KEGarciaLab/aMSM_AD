64cef9fd3af06f2eb8776336cf5f514d153d5ff2
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-03-29 08:31:56 -0500

    Updated for issues #6

--- Utility/Ciftify_Subject_List.sh
+++ Utility/Ciftify_Subject_List.sh
@@ -9,7 +9,7 @@ DATASET=/N/project/ADNI_Processing/ADNI_FS6_ADSP/FINAL_FOR_EXTRACTION # Folder c
 CURRENT_DATETIME=$(date +'%Y-%m-%d_%H-%M-%S') # Date and time will be appended to the text file so multiple runs keep data separate
 OUTPUT_DIR=${HOME}/Scripts/MyScripts/Output/$(basename "$0") # Output location
 OUTPUT_FILE=${OUTPUT_DIR}/subject_numbers_${CURRENT_DATETIME}.txt # Output file name
-SUBJECTS="0056 0059 0069 0127 0156 0173 0210 0229 0296 0377" # Subjects to get full folder name for
+SUBJECTS="0454 0602 0605 0680 0746 0800 0802 0908 1052 1122" # Subjects to get full folder name for
 
 ########## ENSURE THAT OUTPUT AND LOG DIRS EXISTS
 mkdir -p ${LOG_OUTPUT_DIR}
