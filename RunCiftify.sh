3dad59f2aa84b37600b502e26a8ed8f9e6051efb
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-05-11 10:28:32 -0500

    updated to get missing subject

--- RunCiftify.sh
+++ RunCiftify.sh
@@ -10,7 +10,7 @@ DATASET_NAME="ADNI" # Name of dataset being used, must be ADNI or IADRC
 DATASET=/N/project/ADNI_Processing/ADNI_FS6_ADSP/FINAL_FOR_EXTRACTION # Folder containing subject data
 CIFTIFY_OUTPUT_DIR=/N/project/aMSM_AD/ADNI/HCP/TO_BE_PROCESSED # ouptut location for script results
 SCRIPT_OUTPUT_DIR=${HOME}/Scripts/MyScripts/Output/$(basename "$0")/${CURRENT_DATETIME} # Output location for the generated scripts
-SUBJECT_TXT=${HOME}/Scripts/MyScripts/Output/Ciftify_Subject_List.sh/subject_numbers_2024-04-22_09-36-35.txt #List of subjects to run, ensure you have ran Ciftify_Subject_List.sh and are pointng to the correct file
+SUBJECT_TXT=${HOME}/Scripts/MyScripts/Output/Ciftify_Subject_List.sh/subject_numbers_2024-05-11_11-27-39.txt #List of subjects to run, ensure you have ran Ciftify_Subject_List.sh and are pointng to the correct file
 ACCOUNT="r00540" # Slurm allocation to use
 
 ########## ENSURE THAT OUTPUT AND LOG DIRS EXISTS
