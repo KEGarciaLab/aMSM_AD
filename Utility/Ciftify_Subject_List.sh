e142a44d65e29ce18d69462cef32f3e4171851db
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-05-29 12:28:07 -0500

    Updated for the first set of IADRC subjects per issue #14

--- Utility/Ciftify_Subject_List.sh
+++ Utility/Ciftify_Subject_List.sh
@@ -4,12 +4,12 @@ LOG_OUTPUT_DIR=${HOME}/Scripts/MyScripts/logs # Dir for log file
 LOG_OUTPUT=${LOG_OUTPUT_DIR}/$(basename "$0")_${CURRENT_DATETIME}.log # name and location of log file
 
 ########## CHANGE AS NEEDED
-DATASET_NAME="ADNI" # Name of dataset being used, must be ADNI or IADRC
-DATASET=/N/project/ADNI_Processing/ADNI_FS6_ADSP/FINAL_FOR_EXTRACTION # Folder containing subject data
+DATASET_NAME="IADRC" # Name of dataset being used, must be ADNI or IADRC
+DATASET=/N/project/aMSM_AD/IADRC # Folder containing subject data
 CURRENT_DATETIME=$(date +'%Y-%m-%d_%H-%M-%S') # Date and time will be appended to the text file so multiple runs keep data separate
 OUTPUT_DIR=${HOME}/Scripts/MyScripts/Output/$(basename "$0") # Output location
 OUTPUT_FILE=${OUTPUT_DIR}/subject_numbers_${CURRENT_DATETIME}.txt # Output file name
-SUBJECTS="0056" # Subjects to get full folder name for
+SUBJECTS="10149 10150 10151 10152 10153 10154 10155 10552 10560 10561 10564 10566 10568 10573 10576 10579 10581 10584 10589 10592 10599 10602 10605 10606 10607" # Subjects to get full folder name for
 
 ########## ENSURE THAT OUTPUT AND LOG DIRS EXISTS
 mkdir -p ${LOG_OUTPUT_DIR}
