66df4dfd2dd23f8a91965424795ec842b74cc9b1
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-06-12 05:06:22 -0500

    Updated to handle IADRC data

--- RunMSM.sh
+++ RunMSM.sh
@@ -8,7 +8,8 @@ SUBJECTS=() # Array of subject numbers to be processed
 LEVELS=6 # Levels for MSM
 
 ######### CHANGE AS NEEDED
-DATASET=/N/project/aMSM_AD/ADNI/HCP/TO_BE_PROCESSED # Folder containing subject data
+DATASET_NAME="IADRC" # Name of dataset must be eithe ADNI or IADRC
+DATASET=/N/project/aMSM_AD/IADRC_PROCESSING/TO_BE_PROCESSED # Folder containing subject data
 OUTPUT_DIR=${HOME}/Scripts/MyScripts/Output/$(basename "$0")/${CURRENT_DATETIME} # ouptut location for generated scripts
 ACCOUNT="r00540" # Slurm allocation to use
 MAXCP=${DATASET}/ico5sphere.LR.reg.surf.gii # path to ico5sphere
@@ -25,13 +26,21 @@ mkdir -p ${OUTPUT_DIR}
 ########## BEGIN LOGGING
 exec > >(tee -a "${LOG_OUTPUT}") 2>&1
 
+########## DETERMINING STARTING TIMEPOINT
+if [ ${DATASET_NAME} == "IADRC" ]; then
+    STARTING_TIME="1"
+fi
+
+if [ ${DATASET_NAME} == "ADNI" ]; then
+    STARTING_TIME="BL"
+fi 
 ########## GET SUBJECTS
 echo "***************************************************************************"
 echo "LOCATING SUBJECTS"
 echo "***************************************************************************"
 for DIR in "${DATASET}"/*;do
     if [ -d ${DIR} ]; then
-        SUBJECT=$(echo "${DIR}" | grep -oP '(?<=Subject_)\d+(?=_BL)')
+        SUBJECT=$(echo "${DIR}" | grep -oP '(?<=Subject_)\d+(?=_${STARTING_TIME})')
         SUBJECTS+=(${SUBJECT})
     fi
 done
@@ -58,9 +67,9 @@ for SUBJECT in ${SUBJECTS[@]}; do
 
     ########## DEFINE BASLINE FILES AS YOUNGER
     ######## GET DATA LOCATION 
-    DIRECTORIES=("${DATASET}/Subject_${SUBJECT}_BL"/*)
+    DIRECTORIES=("${DATASET}/Subject_${SUBJECT}_${STARTING_TIME}"/*)
     DIRECTORIES=${DIRECTORIES[0]//*zz_templates/}
-    BL_DIR=${DATASET}/Subject_${SUBJECT}_BL/${DIRECTORIES[0]##*/} #Base directory withg baseline data
+    BL_DIR=${DATASET}/Subject_${SUBJECT}_${STARTING_TIME}/${DIRECTORIES[0]##*/} #Base directory withg baseline data
     BL_FULL_DATA=$(basename "${BL_DIR}") # Full subject name for files
     BL_DIR=${BL_DIR}/${RESOLUTION_LOCATION} # Directory with meshes
     echo "YOUNGER DATA LOACTED AT ${BL_DIR}"
@@ -98,9 +107,9 @@ for SUBJECT in ${SUBJECTS[@]}; do
         echo "BEGIN REGISTRATION BETWEEN BL AND ${TIME_POINT}"
 
         ######## CREATE MSM OUTPUT DIRS
-        MSM_F_DIR=${MSM_OUT}/${SUBJECT}_BL_to_${TIME_POINT}
+        MSM_F_DIR=${MSM_OUT}/${SUBJECT}_${STARTING_TIME}_to_${TIME_POINT}
         mkdir -p ${MSM_F_DIR}
-        MSM_R_DIR=${MSM_OUT}/${SUBJECT}_${TIME_POINT}_to_BL
+        MSM_R_DIR=${MSM_OUT}/${SUBJECT}_${TIME_POINT}_to_${STARTING_TIME}
         mkdir -p ${MSM_R_DIR}
         
         ######## GET DATA LOCATION
@@ -159,20 +168,20 @@ for SUBJECT in ${SUBJECTS[@]}; do
             fi
 
             ######## CREATE OUTPUT NAMES
-            F_OUT=${MSM_F_DIR}/${SUBJECT}_${HEMISPHERE}_BL-${TIME_POINT}.
-            R_OUT=${MSM_R_DIR}/${SUBJECT}_${HEMISPHERE}_${TIME_POINT}-BL.
+            F_OUT=${MSM_F_DIR}/${SUBJECT}_${HEMISPHERE}_${STARTING_TIME}-${TIME_POINT}.
+            R_OUT=${MSM_R_DIR}/${SUBJECT}_${HEMISPHERE}_${TIME_POINT}-${STARTING_TIME}.
 
             echo "***************************************************************************"
             echo "BEGIN GENERATING MSM SCRIPTS"
             echo "***************************************************************************"
             ######## FORWARD
-cat > ${OUTPUT_DIR}/Run_${SUBJECT}_${HEMISPHERE}_BL-${TIME_POINT}.sh << EOF
+cat > ${OUTPUT_DIR}/Run_${SUBJECT}_${HEMISPHERE}_${STARTING_TIME}-${TIME_POINT}.sh << EOF
 #!/bin/bash
 
-#SBATCH -J MSM.${SUBJECT}.${HEMISPHERE}.BL-${TIME_POINT}
+#SBATCH -J MSM.${SUBJECT}.${HEMISPHERE}.${STARTING_TIME}-${TIME_POINT}
 #SBATCH -p general
-#SBATCH -o ${HOME}/Scripts/MyScripts/logs/Slurm/%j_MSM_${SUBJECT}_BL-${TIME_POINT}.txt
-#SBATCH -e ${HOME}/Scripts/MyScripts/logs/Slurm/%j_MSM_${SUBJECT}_BL-${TIME_POINT}_error.txt
+#SBATCH -o ${HOME}/Scripts/MyScripts/logs/Slurm/%j_MSM_${SUBJECT}_${STARTING_TIME}-${TIME_POINT}.txt
+#SBATCH -e ${HOME}/Scripts/MyScripts/logs/Slurm/%j_MSM_${SUBJECT}_${STARTING_TIME}-${TIME_POINT}_error.txt
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=sarigdon@iu.edu
 #SBATCH --nodes=1
@@ -205,16 +214,16 @@ wb_command -surface-resample ${OAS} ${OSS} ${F_OUT}sphere.CPgrid.reg.surf.gii "B
 wb_command -surface-distortion ${F_OUT}${HEMISPHERE}YAS.CPgrid.surf.gii ${F_OUT}anat.CPgrid.reg.surf.gii ${F_OUT}surfdist.CPgrid.func.gii
 EOF
 
-            echo "COMPLETED Run_${SUBJECT}_${HEMISPHERE}_BL-${TIME_POINT}.sh"
+            echo "COMPLETED Run_${SUBJECT}_${HEMISPHERE}_${STARTING_TIME}-${TIME_POINT}.sh"
 
             ######## REVERSE
-cat > ${OUTPUT_DIR}/Run_${SUBJECT}_${HEMISPHERE}_${TIME_POINT}-BL.sh << EOF
+cat > ${OUTPUT_DIR}/Run_${SUBJECT}_${HEMISPHERE}_${TIME_POINT}-${STARTING_TIME}.sh << EOF
 #!/bin/bash
 
-#SBATCH -J MSM.${SUBJECT}.${HEMISPHERE}.${TIME_POINT}-BL
+#SBATCH -J MSM.${SUBJECT}.${HEMISPHERE}.${TIME_POINT}-${STARTING_TIME}
 #SBATCH -p general
-#SBATCH -o ${HOME}/Scripts/MyScripts/logs/Slurm/%j_MSM_${SUBJECT}_${TIME_POINT}-BL.txt
-#SBATCH -e ${HOME}/Scripts/MyScripts/logs/Slurm/%j_MSM_${SUBJECT}_${TIME_POINT}-BL_error.txt
+#SBATCH -o ${HOME}/Scripts/MyScripts/logs/Slurm/%j_MSM_${SUBJECT}_${TIME_POINT}-${STARTING_TIME}.txt
+#SBATCH -e ${HOME}/Scripts/MyScripts/logs/Slurm/%j_MSM_${SUBJECT}_${TIME_POINT}-${STARTING_TIME}_error.txt
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=sarigdon@iu.edu
 #SBATCH --nodes=1
@@ -247,12 +256,12 @@ wb_command -surface-resample ${YAS} ${YSS} ${R_OUT}sphere.CPgrid.reg.surf.gii "B
 wb_command -surface-distortion ${R_OUT}${HEMISPHERE}OAS.CPgrid.surf.gii ${R_OUT}anat.CPgrid.reg.surf.gii ${R_OUT}surfdist.CPgrid.func.gii
 EOF
 
-            echo "COMPLETED Run_${SUBJECT}_${HEMISPHERE}_${TIME_POINT}-BL.sh"
+            echo "COMPLETED Run_${SUBJECT}_${HEMISPHERE}_${TIME_POINT}-${STARTING_TIME}.sh"
             echo "***************************************************************************"
             echo "SUBMITING SCRIPTS"
             echo "***************************************************************************"
-            sbatch ${OUTPUT_DIR}/Run_${SUBJECT}_${HEMISPHERE}_BL-${TIME_POINT}.sh
-            sbatch ${OUTPUT_DIR}/Run_${SUBJECT}_${HEMISPHERE}_${TIME_POINT}-BL.sh
+            sbatch ${OUTPUT_DIR}/Run_${SUBJECT}_${HEMISPHERE}_${STARTING_TIME}-${TIME_POINT}.sh
+            sbatch ${OUTPUT_DIR}/Run_${SUBJECT}_${HEMISPHERE}_${TIME_POINT}-${STARTING_TIME}.sh
             echo
         done
     done
