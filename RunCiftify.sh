2809bc1d1d6366ac2f2291de0d129bc3db5777b5
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-06-25 13:25:04 -0500

    Updated for example data

--- RunCiftify.sh
+++ RunCiftify.sh
@@ -10,7 +10,7 @@ DATASET_NAME="ADNI" # Name of dataset being used, must be ADNI or IADRC
 DATASET=/N/slate/sarigdon # Folder containing subject data
 CIFTIFY_OUTPUT_DIR=/N/project/aMSM_AD/IADRC_PROCESSING/TO_BE_PROCESSED # ouptut location for script results
 SCRIPT_OUTPUT_DIR=${HOME}/Scripts/MyScripts/Output/$(basename "$0")/${CURRENT_DATETIME} # Output location for the generated scripts
-SUBJECT_TXT=${HOME}/Scripts/MyScripts/Output/Ciftify_Subject_List.sh/subject_numbers_2024-06-14_17-46-18.txt #List of subjects to run, ensure you have ran Ciftify_Subject_List.sh and are pointng to the correct file
+SUBJECT_TXT=${HOME}/Scripts/MyScripts/Output/Ciftify_Subject_List.sh/subject_numbers_2024-06-25_14-24-36.txt #List of subjects to run, ensure you have ran Ciftify_Subject_List.sh and are pointng to the correct file
 ACCOUNT="r00540" # Slurm allocation to use
 
 ########## ENSURE THAT OUTPUT AND LOG DIRS EXISTS
