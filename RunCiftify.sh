39de43c4cfb1bfda1d8924ec71bb2b8ca9599816
Sammy Rigdon <sammyrigdon496@gmail.com>
2024-07-23 02:49:54 -0500

    Updated to make more flexible per issue #19

--- RunCiftify.sh
+++ RunCiftify.sh
@@ -6,11 +6,14 @@ LOG_OUTPUT_DIR=${HOME}/Scripts/MyScripts/logs
 LOG_OUTPUT=${LOG_OUTPUT_DIR}/$(basename "$0")_${CURRENT_DATETIME}.log # name and location of log file
 
 ######### CHANGE AS NEEDED
+DELIMITER="_" # Delimiter used in name of files to seperate name
+SUBJECT_PLACEMENT="1" # which place the subject id is based on delimiter 1 indicates the space to the left of first delimiter
+TIME_POINT_PLACEMENT="2" # which place holds the time point designation based on delimiter
 DATASET_NAME="IADRC" # Name of dataset being used, must be ADNI or IADRC
 DATASET=/N/project/aMSM_AD/IADRC # Folder containing subject data
 CIFTIFY_OUTPUT_DIR=/N/project/aMSM_AD/IADRC_PROCESSING/TO_BE_PROCESSED # ouptut location for script results
 SCRIPT_OUTPUT_DIR=${HOME}/Scripts/MyScripts/Output/$(basename "$0")/${CURRENT_DATETIME} # Output location for the generated scripts
-SUBJECT_TXT=${HOME}/Scripts/MyScripts/Output/Ciftify_Subject_List.sh/subject_numbers_2024-07-15_15-12-28.txt #List of subjects to run, ensure you have ran Ciftify_Subject_List.sh and are pointng to the correct file
+SUBJECT_TXT=${HOME}/Scripts/MyScripts/Output/Ciftify_Subject_List.sh/subject_numbers_2024-07-23_03-37-47.txt #List of subjects to run, ensure you have ran Ciftify_Subject_List.sh and are pointng to the correct file
 ACCOUNT="r00540" # Slurm allocation to use
 
 ########## ENSURE THAT OUTPUT AND LOG DIRS EXISTS
@@ -32,29 +35,27 @@ while read -r SUBJECT_DIR; do
 done < ${SUBJECT_TXT}
 
 ########## GENERATE SCRIPTS
-######## ADNI DATA
-if [ "${DATASET_NAME}" == "ADNI" ]; then
-    for DIR in ${SUBJECT_DIRS[@]}; do
-        ########## EXTRACT TIME POINTS
-        echo "***************************************************************************"
-        echo "GENERATING SCRIPTS"
-        echo "***************************************************************************"
-        SUBJECT=$(echo "$DIR" | awk -F_ '{print $3}')
-        TIME_POINT=$(echo "$DIR" | awk -F_ '{print $4}')
-        echo "BEGIN GENERATION OF SCRIPT FOR ${SUBJECT} FOR TIME POINT ${TIME_POINT}"
+for DIR in ${SUBJECT_DIRS[@]}; do
+    ########## EXTRACT TIME POINTS
+    echo "***************************************************************************"
+    echo "GENERATING SCRIPTS"
+    echo "***************************************************************************"
+    SUBJECT=$(echo "$DIR" | awk -F"${DELIMITER}" -v pos="${SUBJECT_PLACEMENT}" '{print $pos}')
+    TIME_POINT=$(echo "$DIR" | awk -F"${DELIMITER}" -v pos="${TIME_POINT_PLACEMENT}" '{print $pos}')
+    echo "BEGIN GENERATION OF SCRIPT FOR ${SUBJECT} FOR TIME POINT ${TIME_POINT}"
 
-        ########## CREATE SUBJECT FOLDERS
-        echo "CREATING OUTPUT DIRS"
-        SUBJECT_CIFTIFY_OUTPUT_DIR=${CIFTIFY_OUTPUT_DIR}/Subject_${SUBJECT}_${TIME_POINT}
-        mkdir ${SUBJECT_CIFTIFY_OUTPUT_DIR}
+    ########## CREATE SUBJECT FOLDERS
+    echo "CREATING OUTPUT DIRS"
+    SUBJECT_CIFTIFY_OUTPUT_DIR=${CIFTIFY_OUTPUT_DIR}/Subject_${SUBJECT}_${TIME_POINT}
+    mkdir ${SUBJECT_CIFTIFY_OUTPUT_DIR}
 
-        ######### GENERATE SCRIPT
-        echo "WRITING SCRIPT"
-        echo "SCRIPT COMMAND: ciftify_recon_all --fs-subjects-dir ${DATASET} --ciftify-work-dir ${SUBJECT_CIFTIFY_OUTPUT_DIR} ${DIR}"
-        cat > ${SCRIPT_OUTPUT_DIR}/recon_all_Subject_${SUBJECT}_${TIME_POINT}.sh <<EOF
+    ######### GENERATE SCRIPT
+    echo "WRITING SCRIPT"
+    echo "SCRIPT COMMAND: ciftify_recon_all --fs-subjects-dir ${DATASET} --ciftify-work-dir ${SUBJECT_CIFTIFY_OUTPUT_DIR} ${DIR}"
+    cat > ${SCRIPT_OUTPUT_DIR}/recon_all_Subject_${SUBJECT}_${TIME_POINT}.sh <<EOF
 #!/bin/bash
 
-#SBATCH -J ADNI_recon_all_Subject_${SUBJECT}_${TIME_POINT}
+#SBATCH -J ${DATASET_NAME}_recon_all_Subject_${SUBJECT}_${TIME_POINT}
 #SBATCH -p general
 #SBATCH -o ${HOME}/Scripts/MyScripts/logs/Slurm/%j_Subject_${SUBJECT}_${TIME_POINT}.txt
 #SBATCH -e ${HOME}/Scripts/MyScripts/logs/Slurm/%j_Subject_${SUBJECT}_${TIME_POINT}_error.txt
@@ -68,49 +69,7 @@ if [ "${DATASET_NAME}" == "ADNI" ]; then
 
 ciftify_recon_all --fs-subjects-dir ${DATASET} --ciftify-work-dir ${SUBJECT_CIFTIFY_OUTPUT_DIR} ${DIR}
 EOF
-        ########## SUBMIT SCRIPT TO SLURM
-        echo "SUBMITTING SCRIPT FOR ${SUBJECT} FOR TIME POINT ${TIME_POINT}"
-        sbatch ${SCRIPT_OUTPUT_DIR}/recon_all_Subject_${SUBJECT}_${TIME_POINT}.sh
-    done
-
-######## IADRC DATA
-elif [ "${DATASET_NAME}" == "IADRC" ]; then
-    for DIR in ${SUBJECT_DIRS[@]}; do
-        ########## EXTRACT TIME POINTS
-        echo "***************************************************************************"
-        echo "GENERATING SCRIPTS"
-        echo "***************************************************************************"
-        SUBJECT=$(echo "$DIR" | awk -F_ '{print $1}')
-        TIME_POINT=$(echo "$DIR" | awk -F_ '{print $2}')
-        echo "BEGIN GENERATION OF SCRIPT FOR ${SUBJECT} FOR TIME POINT ${TIME_POINT}"
-
-        ########## CREATE SUBJECT FOLDERS
-        echo "CREATING OUTPUT DIRS"
-        SUBJECT_CIFTIFY_OUTPUT_DIR=${CIFTIFY_OUTPUT_DIR}/Subject_${SUBJECT}_${TIME_POINT}
-        mkdir ${SUBJECT_CIFTIFY_OUTPUT_DIR}
-
-        ######### GENERATE SCRIPT
-        echo "WRITING SCRIPT"
-        echo "SCRIPT COMMAND: ciftify_recon_all --fs-subjects-dir ${DATASET} --ciftify-work-dir ${SUBJECT_CIFTIFY_OUTPUT_DIR} ${DIR}"
-        cat > ${SCRIPT_OUTPUT_DIR}/recon_all_Subject_${SUBJECT}_${TIME_POINT}.sh <<EOF
-#!/bin/bash
-
-#SBATCH -J IADRC_recon_all_Subject_${SUBJECT}_${TIME_POINT}
-#SBATCH -p general
-#SBATCH -o ${HOME}/Scripts/MyScripts/logs/Slurm/%j_Subject_${SUBJECT}_${TIME_POINT}.txt
-#SBATCH -e ${HOME}/Scripts/MyScripts/logs/Slurm/%j_Subject_${SUBJECT}_${TIME_POINT}_error.txt
-#SBATCH --mail-type=fail
-#SBATCH --mail-user=sarigdon@iu.edu
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=49:00:00
-#SBATCH --mem=16G
-#SBATCH -A ${ACCOUNT}
-
-ciftify_recon_all --fs-subjects-dir ${DATASET} --ciftify-work-dir ${SUBJECT_CIFTIFY_OUTPUT_DIR} ${DIR}
-EOF
-        ########## SUBMIT SCRIPT TO SLURM
-        echo "SUBMITTING SCRIPT FOR ${SUBJECT} FOR TIME POINT ${TIME_POINT}"
-        sbatch ${SCRIPT_OUTPUT_DIR}/recon_all_Subject_${SUBJECT}_${TIME_POINT}.sh
-    done
-fi
+    ########## SUBMIT SCRIPT TO SLURM
+    echo "SUBMITTING SCRIPT FOR ${SUBJECT} FOR TIME POINT ${TIME_POINT}"
+    sbatch ${SCRIPT_OUTPUT_DIR}/recon_all_Subject_${SUBJECT}_${TIME_POINT}.sh
+done
