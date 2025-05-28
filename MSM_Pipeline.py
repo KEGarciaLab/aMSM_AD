c1f4922b88347020b0ed7aa2b9cb797df3657ccb
Sammy Rigdon <sammyrigdon496@gmail.com>
2025-05-28 12:10:31 -0500

    fixed resample again

--- MSM_Pipeline.py
+++ MSM_Pipeline.py
@@ -683,17 +683,17 @@ def generate_avg_maps(ciftify_dataset: str, msm_dataset: str, subject: str, youn
 
     # Generate AvgFor Anatomical Surfaces
     print("Begin generating avgfor surfaces")
-    run(f"wb_command -surface-resample {left_older_anatomical_surface} {left_older_spherical_surface} {max_cp} {left_avgfor_cpgrid_sphere} BARYCENTRIC {left_avgfor_cpgrid_anat}", shell=True)
-    run(f"wb_command -surface-resample {right_older_anatomical_surface} {right_older_spherical_surface} {max_cp} {right_avgfor_cpgrid_sphere} BARYCENTRIC {right_avgfor_cpgrid_anat}", shell=True)
-    run(f"wb_command -surface-resample {left_older_anatomical_surface} {left_older_spherical_surface} {max_anat} {left_avgfor_anatgrid_sphere} BARYCENTRIC {left_avgfor_anatgrid_anat}", shell=True)
-    run(f"wb_command -surface-resample {right_older_anatomical_surface} {right_older_spherical_surface} {max_anat} {right_avgfor_anatgrid_sphere} BARYCENTRIC {right_avgfor_anatgrid_anat}", shell=True)
+    run(f"wb_command -surface-resample {left_older_anatomical_surface} {left_older_spherical_surface} {max_cp} {left_avgfor_cpgrid_sphere} \"BARYCENTRIC\" {left_avgfor_cpgrid_anat}", shell=True)
+    run(f"wb_command -surface-resample {right_older_anatomical_surface} {right_older_spherical_surface} {max_cp} {right_avgfor_cpgrid_sphere} \"BARYCENTRIC\" {right_avgfor_cpgrid_anat}", shell=True)
+    run(f"wb_command -surface-resample {left_older_anatomical_surface} {left_older_spherical_surface} {max_anat} {left_avgfor_anatgrid_sphere} \"BARYCENTRIC\" {left_avgfor_anatgrid_anat}", shell=True)
+    run(f"wb_command -surface-resample {right_older_anatomical_surface} {right_older_spherical_surface} {max_anat} {right_avgfor_anatgrid_sphere} \"BARYCENTRIC\" {right_avgfor_anatgrid_anat}", shell=True)
 
     # Generate revfor surfdist
     print("Begin generating revfor surfdist")
-    run(f"wb_command -metric-resample {left_cpgrid_surfdist_reverse} {left_cpgrid_sphere_reverse} {left_revfor_cpgrid_sphere} BARYCENTRIC {left_revfor_cpgrid_surfdist}", shell=True)
-    run(f"wb_command -metric-resample {left_anatgrid_surfdist_reverse} {left_anatgrid_sphere_reverse} {left_revfor_anatgrid_sphere} BARYCENTRIC {left_revfor_anatgrid_surfdist}", shell=True)
-    run(f"wb_command -metric-resample {right_cpgrid_surfdist_reverse} {right_cpgrid_sphere_reverse} {right_revfor_cpgrid_sphere} BARYCENTRIC {right_revfor_cpgrid_surfdist}", shell=True)
-    run(f"wb_command -metric-resample {right_anatgrid_surfdist_reverse} {right_anatgrid_sphere_reverse} {right_revfor_anatgrid_sphere} BARYCENTRIC {right_revfor_anatgrid_surfdist}", shell=True)
+    run(f"wb_command -metric-resample {left_cpgrid_surfdist_reverse} {left_cpgrid_sphere_reverse} {left_revfor_cpgrid_sphere} \"BARYCENTRIC\" {left_revfor_cpgrid_surfdist}", shell=True)
+    run(f"wb_command -metric-resample {left_anatgrid_surfdist_reverse} {left_anatgrid_sphere_reverse} {left_revfor_anatgrid_sphere} \"BARYCENTRIC\" {left_revfor_anatgrid_surfdist}", shell=True)
+    run(f"wb_command -metric-resample {right_cpgrid_surfdist_reverse} {right_cpgrid_sphere_reverse} {right_revfor_cpgrid_sphere} \"BARYCENTRIC\" {right_revfor_cpgrid_surfdist}", shell=True)
+    run(f"wb_command -metric-resample {right_anatgrid_surfdist_reverse} {right_anatgrid_sphere_reverse} {right_revfor_anatgrid_sphere} \"BARYCENTRIC\" {right_revfor_anatgrid_surfdist}", shell=True)
 
     # calculate average surfdist
     print("begin calculating avgfor surfdists")
