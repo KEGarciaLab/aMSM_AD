4be2e1b2143bbd043dcc4c1d207e9dbf904616fd
Sammy Rigdon <sammyrigdon496@gmail.com>
2025-07-05 13:55:16 -0500

    fixed typo

--- MSM_Pipeline.py
+++ MSM_Pipeline.py
@@ -296,9 +296,9 @@ def generate_post_processing_image(subject_directory: str, subject: str, startin
     scene_set_scale = path.join(
         subject_directory, f"{subject}_{starting_time}-{ending_time}_SET-SCALE.scene")
     image_auto_scale = path.join(
-        {subject_directory}, f"{subject}_{starting_time}-{ending_time}.png")
+        subject_directory, f"{subject}_{starting_time}-{ending_time}.png")
     image_set_scale = path.join(
-        {subject_directory}, f"{subject}_{starting_time}-{ending_time}_SET-SCALE.png")
+        subject_directory, f"{subject}_{starting_time}-{ending_time}_SET-SCALE.png")
     run(f"wb_command -show-scene {scene_auto_scale} {image_auto_scale} 1024 512", shell=True)
     run(f"wb_command -show-scene {scene_set_scale} {image_set_scale} 1024 512", shell=True)
 
